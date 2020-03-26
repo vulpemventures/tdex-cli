c7af137bf6a33bf79f1a692875903dccf792ae22
Marco Argentieri <marco@vulpem.com>
2020-03-26 12:39:42 +0100

    Initial commit

---
+++ ts_src/wallet.ts
@@ -0,0 +1,72 @@
+import { ECPair, networks, payments, Psbt } from 'liquidjs-lib';
+//Types
+import { ECPairInterface } from 'liquidjs-lib/types/ecpair';
+import { Network } from 'liquidjs-lib/types/networks';
+
+export interface WalletInterface {
+  keyPair: ECPairInterface;
+  privateKey: Buffer;
+  publicKey: string;
+  address: string;
+  network: Network;
+  sign(psbt: string): string;
+}
+
+export default class Wallet implements WalletInterface {
+  
+  keyPair: ECPairInterface;
+  privateKey: Buffer;
+  
+  publicKey: string;
+  address: string;
+  network: Network;
+
+  constructor(args) {
+    const { network, keyPair } = args; 
+
+    if (!keyPair)
+      this.keyPair = ECPair.makeRandom({ 
+        network: network ? networks[network] : networks.liquid 
+      });
+    else
+      this.keyPair = keyPair;
+    
+    this.privateKey = this.keyPair.privateKey;
+    this.network = this.keyPair.network;
+    this.address = payments.p2wpkh({ pubkey: this.keyPair.publicKey, network: this.network }).address;
+  }
+
+  sign(psbtBase64: string): string {
+    let psbt 
+    try {
+      psbt = Psbt.fromBase64(psbtBase64);
+    } catch(ignore) {
+      throw(new Error('Invalid psbt'));
+    }
+
+    psbt.signAllInputs(this.keyPair);
+    
+    if (!psbt.validateSignaturesOfAllInputs())
+      throw new Error('Invalid signature');
+    
+    psbt.finalizeAllInputs();
+
+    return psbt.toBase64();
+  }
+}
+
+export function fromWIF(wif: string, network?: string): WalletInterface {
+
+  const _network = network ? networks[network] : networks.liquid 
+
+  try {
+
+    const keyPair = ECPair.fromWIF(wif, _network);
+    return new Wallet({ keyPair });
+
+  } catch(ignore) {
+
+    throw new Error('Invalid keypair');
+
+  }
+}

8073e945e13a739b5cde73ad1551c98c96eb5d30
Marco Argentieri <marco@vulpem.com>
2020-04-08 19:39:03 +0200

    Use JSON for mocked markets

--- src/actions/connectAction.ts
+++ src/actions/connectAction.ts
@@ -4,6 +4,8 @@ import { isValidUrl } from '../helpers';
 
 import State from '../state';
 import { networks } from 'liquidjs-lib';
+
+const markets = require("../markets.example.json")
 const state = new State();
 
 export default function (endpoint: string): void {
@@ -14,16 +16,7 @@ export default function (endpoint: string): void {
 
   // TODO: Connect to provided endpoint and fetch the available pairs
   // client.Markets().then()
-  const markets = {
-    "LBTC-USDT": {
-      "LBTC": "5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225",
-      "USDT": "b66f2b61a8140ba67265c25f6a5d5c836e9c789ee7a172c1408eea37d9d17ab0"
-    },
-    "LBTC-EQUI": {
-      "LBTC": "5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225",
-      "EQUI": "1fd91e7be0f4cefd52853c33475d2947cf5173d5add03de8aa369352852a2b9c"
-    }
-  };
+
 
   const pairs = Object.keys(markets)
 

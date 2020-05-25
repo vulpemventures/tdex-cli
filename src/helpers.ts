f854aad9ad3e854f72076405c58595362801b63d
Marco Argentieri <marco@vulpem.com>
2020-05-25 13:08:36 +0200

    Use @grpc/grpc-js (#13)

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import axios from 'axios';
 import { URL } from 'url';
-import * as grpc from 'grpc';
+import * as grpc from '@grpc/grpc-js';
 
 import * as services from 'tdex-protobuf/js/operator_grpc_pb';
 import * as messages from 'tdex-protobuf/js/operator_pb';

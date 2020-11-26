1ce9a9a0b98d18c03e56a725883d408bb5eff1c5
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2020-11-26 11:44:07 +0100

    Improve getWalletInfo format

--- src/helpers.ts
+++ src/helpers.ts
@@ -19,16 +19,16 @@ export const TAXI_API_URL = {
 
 export function getWalletInfo(addresses: AddressInterface[]): string {
   if (addresses.length === 0)
-    return '0 addresses in your wallet: try "wallet address" to generate new one';
+    return '0 addresses in your wallet: use "wallet address" to generate new one';
 
-  let walletInfo = 'Wallet addresses:\n';
+  let walletInfo = 'Wallet addresses:\n\n';
 
   addresses.forEach(
     (
       { blindingPrivateKey, confidentialAddress }: AddressInterface,
       index: number
     ) => {
-      walletInfo += ` ${index}- address: ${confidentialAddress}, blinding private key: ${blindingPrivateKey}\n`;
+      walletInfo += `index: ${index}\naddress: ${confidentialAddress}\nblinding: ${blindingPrivateKey}\n\n`;
     }
   );
 
